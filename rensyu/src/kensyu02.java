276358a88eacbcbbff84a3ca54a2f87932ae68e4
github yusuke-tominaga-kccs <yusuke-tominaga@kccs.co.jp>
2025-06-05 14:13:13 +0900

    とちゅうけいか

--- rensyu/src/kensyu02.java
+++ rensyu/src/kensyu02.java
@@ -1,20 +1,35 @@
 public class kensyu02 {
 
 	public static void main(String[] args) {
-		double tousigaku = 3;
-		double haitou = 0.02;
+		System.out.println("京セラ持ち株投資会で得られる利益を計算するプログラムです");
+		System.out.print("毎月何円積み立てるか入力してください(３万円→3)>");
+		double tousigaku = new java.util.Scanner(System.in).nextDouble();
+		System.out.println();
+		
+		System.out.print("配当利回りを入力してください(1% → 0.01)>");
+		double haitou = new java.util.Scanner(System.in).nextDouble();
+		System.out.println();
+		
+		System.out.print("何年運用するか入力してください>");
+		int nen = new java.util.Scanner(System.in).nextInt();
+		System.out.println();
+		
 		final int tuki=18;
 		double soutosi = 0;
 		double zikotosi = 0;
 		double rieki = 0;
+		double nengaku = tuki * (tousigaku + (tousigaku/10));
 		
-		for(int i = 0; i < 40; i++) {
-			zikotosi += tousigaku*tuki;
+		for(int i = 0; i < nen; i++) {
+			zikotosi += tuki * tousigaku;
 			if(i == 0) {
-				soutosi += zikotosi+ (zikotosi/10);
+				soutosi += nengaku;
+				rieki += tousigaku/10;
+			}else {
+				rieki += ((soutosi + nengaku)*(haitou)) + tousigaku/10;
+				soutosi = (soutosi + nengaku)*(1+haitou);
 			}
-			soutosi += (zikotosi + soutosi)*haitou + zikotosi + (zikotosi/10);
-			rieki += (soutosi*haitou) + tousigaku/10;
+			
 		}
 		
 		
